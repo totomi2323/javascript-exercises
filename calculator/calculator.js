dc527c8ef8fe611498f8039396ffe0c8a57c9c7e
Tamas Toth <tothtamas2323@gmail.com>
2021-05-05 12:28:00 +0100

    Calculator exercise complete

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,45 @@
-function add () {
-	
+function add (a, b) {
+return (a)+(b);
 }
 
-function subtract () {
-	
+function subtract (a,b) {
+return (a)-(b);
 }
 
-function sum () {
-	
+function sum (a)  {
+	let total = 0;
+	for (let key in a) {
+		total = total + a[key];
+	};
+	return total;
 }
 
-function multiply () {
-	
+function multiply (a) {
+	let total = 1;
+	for (let key in a) {
+		total = total * a[key];
+	};
+	return total;
 }
 
-function power() {
-	
+function power(a,b) {
+	let total = 1;
+	for (let i= 1 ; i <=b ; i++) {
+		total = total * a;
+	}
+	return total;
 }
 
-function factorial() {
-	
+function factorial(a) {
+	total = a ;
+	if (a == 0) { 
+		return 1;} 
+	else { 	
+		for (let i = a; i >= 2; i--) {
+		total = total * (i-1);
+	}
+	return total;
+}
 }
 
 module.exports = {
