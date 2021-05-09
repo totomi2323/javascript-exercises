a900d3c9b27745a4864b8ee75057745d1e9f78dc
Tamas Toth <tothtamas2323@gmail.com>
2021-05-09 15:25:09 +0100

    Fibonaccio exercise complete

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(target) {
+let j = target;
+let fibo = [0,1];
+let newNumber = 0;
+if (j <= 0) { return "OOPS"}
+else { 
+for (i=1; i < j; i++ ) {
+  newNumber = fibo[i] + fibo[i-1] ;
+  fibo = fibo.concat(newNumber);
 }
-
+return fibo[j];}
+}
 module.exports = fibonacci
