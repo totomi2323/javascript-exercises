db73718d84910e94b25c4a2f0b5f5841d663f9cc
Tamas Toth <tothtamas2323@gmail.com>
2021-05-09 14:38:14 +0100

    Palindromes complete

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function() {
+const palindromes = function(word) {
+word = word.toLowerCase();
+let removeSpace = word.split(" ").join("")
+                        .replace(/[^\w\s]|_/g, "");
+let reversedWord = "";
 
+for (let i=removeSpace.length-1 ; i>= 0; i-- ) {
+    reversedWord += removeSpace[i];
 }
 
+if (removeSpace == reversedWord) 
+    {return true;}
+    else 
+    {return false;}
+}
 module.exports = palindromes
