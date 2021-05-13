0748b5dd5614b94d30ea33a99e7f711ff15a7e9f
Tamas Toth <tothtamas2323@gmail.com>
2021-05-13 17:07:23 +0100

    getTheOldest finished using solution, arrays to be looked up again

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-let findTheOldest = function() {
+let findTheOldest = function(people) {
+ /*
+const oldest = people.sort((a,b) => {
+    const lastGuy = a.yearOfDeath - a.yearOfBirth;
+    const nextGuy = b.yearOfDeath - b.yearOfBirth;
+    return lastGuy > nextGuy ? -1 : 1;
+})
+return oldest[0];
+*/
 
+    
+return people.reduce((oldest,recentPerson) => {
+oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath)
+currentAge = getAge(recentPerson.yearOfBirth, recentPerson.yearOfDeath)
+return oldestAge < currentAge ? recentPerson : oldest
+})
+
+function getAge(birth,death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death-birth;
 }
 
+}
 module.exports = findTheOldest
